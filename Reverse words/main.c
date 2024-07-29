9eb52c25efad02c56bc731ea4d3d4c002702e2c0
oddstap <matthewrafei1@gmail.com>
2024-07-29 16:07:17 -0500

    Working on reverse words

--- Reverse words/main.c
+++ Reverse words/main.c
@@ -4,44 +4,91 @@
 
 char* reverseWords(const char* text)
 {
-    char* buffer = (char*)malloc(sizeof(char) * strlen(text));
-    memset(buffer, '\0', strlen(buffer));
-    memcpy(buffer, text, strlen(text));
+    char* tmp = (char*)malloc(sizeof(char) * strlen(text));
+    memset(tmp, '\0', strlen(text));
 
-    char* tmp = buffer;
-    char c = '\0';
+    char* submit = (char*)malloc(sizeof(char) * strlen(text));
+    memset(submit, '\0', strlen(text));
 
-    while(*tmp) {
-        tmp++;
-        if(*tmp == '\0') {
-            for(int i = 0, j = strlen(tmp) - 1; i < j; i++, j--){
-                c = buffer[i];
-                buffer[i] = buffer[j];
-                buffer[j] = c;
+    char c = ' ';
+    //strncat(tmp, &(*text), 1);
+
+    while(*text) {
+        //text++;
+
+        if(*text == '\0') {
+            for(int i = 0, j = strlen(tmp) - 1; i <= j; i++, j--) {
+                c = tmp[i];
+                tmp[i] = tmp[j];
+                tmp[j] = c;
             }
+            strncat(submit, tmp, strlen(tmp));
         }
-        else if (*tmp == ' ') {
-            for(int i = 0, j = strlen(tmp) - 1; i < j; i++, j--){
-                c = buffer[i];
-                buffer[i] = buffer[j];
-                buffer[j] = c;
+
+        if(*text == ' ') {
+            for(int i = 0, j = strlen(tmp) - 1; i <= j; i++, j--) {
+                c = tmp[i];
+                tmp[i] = tmp[j];
+                tmp[j] = c;
             }
-            buffer = tmp + 1;
+            strncat(submit, tmp, strlen(tmp));
+
+            #if 0
+            if(*text++ == ' ') {
+
+                //printf("1 What is %c\n", *text++);
+
+                strncat(submit, "  ", strlen(submit));
+                memset(tmp, '\0', strlen(text));
+                //text++;
+            }
+            else {
+
+                //printf("2 What is %c\n", *text++);
+
+                printf("1");
+
+                strncat(submit, " ", strlen(submit));
+                memset(tmp, '\0', strlen(text));
+                //text++;
+            }
+            #endif
+
+            //printf("3 What is %c\n", *text++);
+
+            //strncat(submit, " ", strlen(submit));
+
+            //memset(tmp, '\0', strlen(text));
+            //text++;
+            //printf("\n");
         }
+
+
+        //printf("The size of tmp is %d\n", (int)strlen(tmp));
+        strncat(tmp, &(*text), 1);
+        text++;
+
+
+        //printf("%s\n", tmp);
     }
 
 
 
-
-    return buffer;
+    return submit;
 }
 
 
 int main(void)
 {
-    char *s = "This is an example!";
+    char *s1 = "The quick brown fox jumps over the lazy dog.";
 
-    printf("%s\n", reverseWords(s));
+    char *s2 = "a b c d";
+
+    char *s3 = "double  spaced  words";
+
+    printf("%s\n", reverseWords(s1));
+    printf("%s\n", reverseWords(s2));
+    printf("%s\n", reverseWords(s3));
 
     return 0;
 }
