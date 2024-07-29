33eb204eda719ca1fa6c847f203416fdb41f9805
oddstap <matthewrafei1@gmail.com>
2024-07-28 23:12:51 -0500

    Working on reverse word problem

--- Reverse words/main.c
+++ Reverse words/main.c
@@ -2,32 +2,36 @@
 #include <stdio.h>
 #include <string.h>
 
-int findEnd(char* s) {
-    int index = 0;
-
-    while(*s && *s != ' '){
-        s++;
-        index++;
-    }
-
-    return index;
-}
-
 char* reverseWords(const char* text)
 {
     char* buffer = (char*)malloc(sizeof(char) * strlen(text));
     memset(buffer, '\0', strlen(buffer));
     memcpy(buffer, text, strlen(text));
-    char c = ' ';
-    int index = 0;
 
-    index = findEnd(buffer+index);
+    char* tmp = buffer;
+    char c = '\0';
 
-    for(int i = 0, j = index - 1; i < j; i++, j--){
-        c = buffer[i];
-        buffer[i] = buffer[j];
-        buffer[j] = c;
+    while(*tmp) {
+        tmp++;
+        if(*tmp == '\0') {
+            for(int i = 0, j = strlen(tmp) - 1; i < j; i++, j--){
+                c = buffer[i];
+                buffer[i] = buffer[j];
+                buffer[j] = c;
+            }
+        }
+        else if (*tmp == ' ') {
+            for(int i = 0, j = strlen(tmp) - 1; i < j; i++, j--){
+                c = buffer[i];
+                buffer[i] = buffer[j];
+                buffer[j] = c;
+            }
+            buffer = tmp + 1;
+        }
     }
+
+
+
 
     return buffer;
 }
