a34a9f596b9dad8ca6030bae10bec74b67998a48
MatthewRafei <matthewrafei1@gmail.com>
2024-07-29 19:50:14 -0500

    Finished Reverse words

--- Reverse words/main.c
+++ Reverse words/main.c
@@ -2,77 +2,53 @@
 #include <stdio.h>
 #include <string.h>
 
+char* reverseString(char* string)
+{
+    char* s = (char*)malloc(sizeof(char) * strlen(string));
+    memset(s, '\0', strlen(s));
+    strcpy(s, string);
+
+    char c = ' ';
+
+    for(int i = 0, j = (int)strlen(s) - 1; i <= j; i++, j--) {
+        c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
+
+    return s;
+}
+
 char* reverseWords(const char* text)
 {
-    char* tmp = (char*)malloc(sizeof(char) * strlen(text));
-    memset(tmp, '\0', strlen(text));
-
     char* submit = (char*)malloc(sizeof(char) * strlen(text));
     memset(submit, '\0', strlen(text));
 
-    char c = ' ';
-    //strncat(tmp, &(*text), 1);
+    char* tmp = (char*)malloc(sizeof(char) * strlen(text));
+    memset(tmp, '\0', sizeof(text));
+
 
     while(*text) {
-        //text++;
-
-        if(*text == '\0') {
-            for(int i = 0, j = strlen(tmp) - 1; i <= j; i++, j--) {
-                c = tmp[i];
-                tmp[i] = tmp[j];
-                tmp[j] = c;
-            }
-            strncat(submit, tmp, strlen(tmp));
-        }
 
         if(*text == ' ') {
-            for(int i = 0, j = strlen(tmp) - 1; i <= j; i++, j--) {
-                c = tmp[i];
-                tmp[i] = tmp[j];
-                tmp[j] = c;
+            strncat(submit, reverseString(tmp), strlen(tmp));
+
+            if(*text+1 == ' ') {
+                strncat(submit, "  ", strlen(text));
             }
-            strncat(submit, tmp, strlen(tmp));
-
-            #if 0
-            if(*text++ == ' ') {
-
-                //printf("1 What is %c\n", *text++);
-
-                strncat(submit, "  ", strlen(submit));
-                memset(tmp, '\0', strlen(text));
-                //text++;
+            else{
+                strncat(submit, " ", strlen(text));
             }
-            else {
-
-                //printf("2 What is %c\n", *text++);
-
-                printf("1");
-
-                strncat(submit, " ", strlen(submit));
-                memset(tmp, '\0', strlen(text));
-                //text++;
-            }
-            #endif
-
-            //printf("3 What is %c\n", *text++);
-
-            //strncat(submit, " ", strlen(submit));
-
-            //memset(tmp, '\0', strlen(text));
-            //text++;
-            //printf("\n");
+            memset(tmp, '\0', strlen(text));
         }
-
-
-        //printf("The size of tmp is %d\n", (int)strlen(tmp));
-        strncat(tmp, &(*text), 1);
+        else {
+            strncat(tmp, &(*text), 1);
+        }
         text++;
-
-
-        //printf("%s\n", tmp);
     }
 
-
+    strncat(submit, reverseString(tmp), strlen(tmp));
+    memset(tmp, '\0', strlen(tmp));
 
     return submit;
 }
@@ -80,15 +56,26 @@ char* reverseWords(const char* text)
 
 int main(void)
 {
+    char *s0 = "This is an example!";
+
     char *s1 = "The quick brown fox jumps over the lazy dog.";
 
     char *s2 = "a b c d";
 
     char *s3 = "double  spaced  words";
 
+    char *s4 = "This is god domain child and I will hunt you";
+
+    char *s5 = "Antidisestablishmentarianism";
+
+    char *s6 = "_vM;#4?0 )u_O % '_/$zvQu3\"P~4w:61J:tW ";
+
+    printf("%s\n", reverseWords(s0));
     printf("%s\n", reverseWords(s1));
     printf("%s\n", reverseWords(s2));
     printf("%s\n", reverseWords(s3));
+    printf("%s\n", reverseWords(s4));
+    printf("%s\n", reverseWords(s5));
 
     return 0;
 }
---
+++ Tap Code Translation/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// for the return string value
+// assign characters *tap_code
+//
+
+char*
+
+void tap_code_translation(const char *text, char *tap_code) {
+
+    //  <----  hajime!
+
+}
+
+int main(void) {
+
+    printf(" ");
+
+
+
+    return 0;
+}
